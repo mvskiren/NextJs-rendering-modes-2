4186fbd4ed1bafd16e4c8698ad7512f832a47afe
Sai Kiran Marisetti <kiranmvs07@gmail.com>
2022-10-17 21:10:29 +0530

    Third commit SSG

--- pages/pokemon/[id].js
+++ pages/pokemon/[id].js
@@ -4,7 +4,21 @@ import Head from "next/head";
 import styles from "../../styles/Home.module.css";
 // import { useState, useEffect } from "react";
 
-export async function getServerSideProps({ params }) {
+export async function getStaticPaths() {
+  const resp = await fetch(
+    "https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json"
+  );
+  const pokemon = await resp.json();
+
+  return {
+    paths: pokemon.map((pokemon) => ({
+      params: { id: pokemon.id.toString() },
+    })),
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params }) {
   const resp = await fetch(
     `https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${params.id}.json`
   );
